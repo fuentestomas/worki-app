f010c632d9ec63b4c706c7744541d533840eb164
fuentestomas <tomas.e.fuentes@gmail.com>
2024-06-28 01:24:26 -0300

    Create cloud server

---
+++ api/index.js
@@ -0,0 +1,20 @@
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * const {onCall} = require("firebase-functions/v2/https");
+ * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
+
+const {onRequest} = require("firebase-functions/v2/https");
+const logger = require("firebase-functions/logger");
+const {app} = require('./app');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://dbuser:worki@workicluster.odp9wzi.mongodb.net/worki');
+
+// Create and deploy your first functions
+// https://firebase.google.com/docs/functions/get-started
+
+exports.app = onRequest(app);
