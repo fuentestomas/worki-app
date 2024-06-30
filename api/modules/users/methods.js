32137eb9e9f2e37e9d73d27c1c409545d9b8cd67
fuentestomas <tomas.e.fuentes@gmail.com>
2024-06-30 18:21:24 -0300

    add dotenv

--- api/modules/users/methods.js
+++ api/modules/users/methods.js
@@ -3,8 +3,22 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const admin = require('firebase-admin');
 const firebaseAuth = require("firebase-admin/auth");
+require('dotenv').config()
 
-const serviceAccount = require("../../firebase/worki.json");
+//const serviceAccount = require("../../firebase/worki.json");
+const serviceAccount = {
+    type: process.env.TYPE,
+    project_id: process.env.PROJECT_ID,
+    private_key_id: process.env.PRIVATE_KEY_ID,
+    private_key: process.env.PRIVATE_KEY,
+    client_email: process.env.CLIENT_EMAIL,
+    client_id: process.env.CLIENT_ID,
+    auth_uri: process.env.AUTH_URI,
+    token_uri: process.env.TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_x509_CERT_URL,
+    client_x509_cert_url: process.env.CLIENT_X509_CERT_URL,
+    universe_domain: process.env.UNIVERSE_DOMAIN
+}
 
 const firebase = admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
