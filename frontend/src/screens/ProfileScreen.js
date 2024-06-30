ce4f59920cfd8b653e46584c663c6d0f74bbff6c
Sebastian <sponce@itlatingroup.cl>
2024-06-30 16:13:53 -0300

    se agrega datos reales a mi perfil

--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   SafeAreaView,
   ScrollView,
@@ -9,99 +9,148 @@ import {
   StyleSheet,
 } from "react-native";
 import { Colors } from "react-native-ui-lib";
-import back from "../assets/img/back.png";
 import logout from "../assets/img/logout.png";
 import worker from "../assets/img/worker.png";
 import { signOutFromGoogle } from "../firebase/googleProvider";
+import { getUserInfo } from "../services/user";
+import LoaderKit from "react-native-loader-kit";
+import { loadFromLocalStorage } from "../hooks/useLocalStorage";
 
+const user = {
+  photoURL: worker,
+  firstName: "John",
+  lastName: "Doe",
+  jobTitle: "Mozo",
+  location: "Buenos Aires, Argentina",
+  email: "john.doe@example.com",
+  phone: "+1234567890",
+  expectedSalary: "$2000 - $3000",
+  fieldOfInterest: "Gastronomía",
+};
 const ProfileScreen = ({ navigation }) => {
-  const user = {
-    photoURL: worker,
-    firstName: "John",
-    lastName: "Doe",
-    jobTitle: "Mozo",
-    location: "Buenos Aires, Argentina",
-    email: "john.doe@example.com",
-    phone: "+1234567890",
-    expectedSalary: "$2000 - $3000",
-    fieldOfInterest: "Gastronomía",
+  const [user, setUser] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = async () => {
+    const { id } = await loadFromLocalStorage("auth");
+    const data = await getUserInfo(id);
+    if (data) {
+      setUser(data);
+      setIsLoading(false);
+      console.log("data:", data);
+    }
   };
 
   return (
     <SafeAreaView style={styles.safeArea}>
-      <ScrollView contentContainerStyle={styles.scrollViewContent}>
-        <Text
-          style={{
-            color: "black",
-            fontSize: 21,
-            marginLeft: 10,
-            marginTop: 10,
-            fontWeight: 500,
-            fontFamily: "Avenir-Black",
-          }}
+      {isLoading ? (
+        <View
+          style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
         >
-          Mi perfil
-        </Text>
-        <View style={styles.container}>
-          <View style={styles.profileContainer}>
-            <Image source={user.photoURL} style={styles.profileImage} />
-            <View style={styles.infoContainer}>
-              <Text
-                style={styles.name}
-              >{`${user.firstName} ${user.lastName}`}</Text>
-              {user.jobTitle && (
-                <Text style={styles.jobTitle}>{user.jobTitle}</Text>
-              )}
-              <Text style={styles.location}>{user.location}</Text>
-            </View>
-          </View>
-          <View style={styles.section}>
-            <Text style={styles.sectionTitle}>Datos de contacto</Text>
-            <View style={styles.sectionContent}>
-              <Text style={styles.sectionText}>{user.email}</Text>
-              <Text style={styles.sectionText}>{user.phone}</Text>
-            </View>
-          </View>
-          <View style={styles.section}>
-            <Text style={styles.sectionTitle}>Información adicional</Text>
-            <View style={styles.sectionContent}>
-              <Text style={styles.sectionText}>
-                <Text
-                  style={{
-                    color: "black",
-                    fontWeight: 500,
-                    fontFamily: "Avenir-Black",
-                  }}
-                >
-                  Salario pretendido:
-                </Text>
-                {` ${user.expectedSalary}`}
-              </Text>
-              <Text style={styles.sectionText}>
-                <Text
-                  style={{
-                    color: "black",
-                    fontWeight: 500,
-                    fontFamily: "Avenir-Black",
-                  }}
-                >
-                  Rubro de interés:
-                </Text>
-                {` ${user.fieldOfInterest}`}
-              </Text>
-            </View>
-          </View>
-          <TouchableOpacity
-            style={styles.logoutButton}
-            onPress={() => {
-              signOutFromGoogle(navigation);
+          <LoaderKit
+            style={{ width: 90, height: 90 }}
+            name={"BallClipRotate"}
+            color={Colors.blue30}
+          />
+        </View>
+      ) : (
+        <ScrollView contentContainerStyle={styles.scrollViewContent}>
+          <Text
+            style={{
+              color: "black",
+              fontSize: 21,
+              marginLeft: 10,
+              marginTop: 10,
+              fontWeight: 500,
+              fontFamily: "Avenir-Black",
             }}
           >
-            <Image source={logout} style={styles.logoutImage} />
-            <Text style={styles.logoutText}>Cerrar sesión</Text>
-          </TouchableOpacity>
-        </View>
-      </ScrollView>
+            Mi perfil
+          </Text>
+          <View style={styles.container}>
+            <View style={styles.profileContainer}>
+              <Image
+                source={{
+                  uri: `data:image/jpeg;base64,${user.photo}`,
+                }}
+                style={styles.profileImage}
+              />
+              <View style={styles.infoContainer}>
+                <Text style={styles.name}>{`${user.fullName}`}</Text>
+                {user.job && (
+                  <Text style={styles.jobTitle}>{user.job}</Text>
+                )}
+                <Text style={styles.location}>{user.address}</Text>
+              </View>
+            </View>
+            <View style={styles.section}>
+              <Text style={styles.sectionTitle}>Datos de contacto</Text>
+              <View style={styles.sectionContent}>
+                <Text style={styles.sectionText}>{user.emailAddress}</Text>
+                <Text style={styles.sectionText}>+{user.phoneNumber}</Text>
+              </View>
+            </View>
+            <View style={styles.section}>
+              <Text style={styles.sectionTitle}>Información adicional</Text>
+              <View style={styles.sectionContent}>
+                {user.roles[0] === "worker" ? (
+                  <Text style={styles.sectionText}>
+                    <Text
+                      style={{
+                        color: "black",
+                        fontWeight: 500,
+                        fontFamily: "Avenir-Black",
+                      }}
+                    >
+                      Salario pretendido:
+                    </Text>
+                    {` ${user.avgPayRate}`}
+                  </Text>
+                ) : (
+                  <Text style={styles.sectionText}>
+                    <Text
+                      style={{
+                        color: "black",
+                        fontWeight: 500,
+                        fontFamily: "Avenir-Black",
+                      }}
+                    >
+                      Rango de salarios:
+                    </Text>
+                    {` ${user.avgPayRate}`}
+                  </Text>
+                )}
+                {user.roles[0] === "business" &&
+                  <Text style={styles.sectionText}>
+                    <Text
+                      style={{
+                        color: "black",
+                        fontWeight: 500,
+                        fontFamily: "Avenir-Black",
+                      }}
+                    >
+                      Rubro:
+                    </Text>
+                    {` ${user.category}`}
+                  </Text>
+                }
+              </View>
+            </View>
+            <TouchableOpacity
+              style={styles.logoutButton}
+              onPress={() => {
+                signOutFromGoogle(navigation);
+              }}
+            >
+              <Image source={logout} style={styles.logoutImage} />
+              <Text style={styles.logoutText}>Cerrar sesión</Text>
+            </TouchableOpacity>
+          </View>
+        </ScrollView>
+      )}
     </SafeAreaView>
   );
 };
